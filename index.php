396478006f2eeeae85d4227b632f476b81aeb9f2
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-08 23:22:31 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hellow World!";
+
+?>
+
+<script>
+   var conn = new WebSocket('ws://localhost:8080');
+   conn.onopen = function(e) {
+      console.log("Connection established!");
+   };
+
+   conn.onmessage = function(e) {
+      console.log(e.data);
+   };
+</script>
